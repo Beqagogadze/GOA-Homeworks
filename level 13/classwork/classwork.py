ceb45d03bc0c8c31dff0c0936d6cbffab337e93e
Beqagogadze <beqaghoghadze777@gmail.com>
2024-07-13 15:14:59 +0400

    first commit

---
+++ level 13/classwork/classwork.py
@@ -0,0 +1,19 @@
+#print("Hello world")
+#print("Hello world")
+#print("Hello world")
+#print("Hello world")
+#print("Hello world")
+#print("Hello world")
+#print("Hello world")
+#print("Hello world")
+#print("Hello world")
+#print("Hello world")
+
+
+print
+print
+
+input("str") + (str + 10+23)
+
+print=name(Beqa)
+print=age(15)
