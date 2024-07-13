6f7234307d070db7ae24dabbecb73ca3faf05cf9
Beqagogadze <beqaghoghadze777@gmail.com>
2024-07-13 16:26:15 +0400

    first commit

---
+++ GOA/level 14/homework/homework.py
@@ -0,0 +1,14 @@
+print(10 < 11 and 15 > 2)
+print(20 < 11 and 30 > 2)
+print(40 < 11 and 15 > 2)
+print(60 < 11 and 15 > 2)
+print(70 < 11 and 15 > 2)
+print(80 < 11 and 15 > 2)
+print(90 < 11 and 25> 2)
+print(100 < 11 and 11 > 2)
+print(110 < 11 and 12 > 2)
+print(120 < 11 and 35 > 2)
+print(130 < 11 and 45 > 2)
+print(140 < 11 and 60 > 2)
+
+
